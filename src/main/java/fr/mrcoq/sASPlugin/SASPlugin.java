5ad5dfbb7e198c275eeed0bca4b7d2835b06727b
ValentinJDT <vjeandot@yahoo.com>
2025-06-29 21:12:39 +0200

    first commit

---
+++ src/main/java/fr/mrcoq/sASPlugin/SASPlugin.java
@@ -0,0 +1,39 @@
+package fr.mrcoq.sASPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+public final class SASPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+            if(isShutdown("<ton lien>")) {
+                Bukkit.shutdown();
+            }
+        });
+    }
+
+    public boolean isShutdown(String link) {
+        try {
+            Document doc = Jsoup.connect(link).get();
+            for(Element element : doc.getElementsByClass("wsite-content-title")) {
+                String content = element.html();
+                if(content.contains("shutdown=")) {
+                    String shutdownValue = content.split("shutdown=")[1].split("\"")[0];
+                    return shutdownValue.equalsIgnoreCase("true");
+                }
+            }
+
+        } catch(IOException e) {
+            Bukkit.getLogger().log(Level.SEVERE, "Error while checking shutdown status", e);
+        }
+        return false;
+    }
+}
